09b77ad75fcbfa1d0b01865ce95627c8093ee85d
srmbshishir <42672357+srmbshishir@users.noreply.github.com>
2021-04-24 08:46:07 +0600

    Add files via upload

---
+++ midproject - Copy/viewer/admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../style/admin.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>     
+    <ul>
+    <li><button onclick="location.href='addemployee.php';">Add employee</button></li>
+    <li><button onclick="location.href = 'livesearch.php';">Search employee</button></li>
+    <li><button onclick="location.href = 'showAllEmployee.php';">Show All Employee</button></li>
+    <li><button onclick="location.href = 'livesearch.php';">show daily sales</button></li>
+    <li><button onclick="location.href = 'monthlysale.php';">show monthly sales</button></li>
+    <li><button onclick="location.href = 'productdetails.php';">show product details</button></li>
+    <li><button onclick="location.href = 'dashboard.php';">go back</button></li>
+    </nav>        
+    </script>
+    
+
+</body>
+</html>
